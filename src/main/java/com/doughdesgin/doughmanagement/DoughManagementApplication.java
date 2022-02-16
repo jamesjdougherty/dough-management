c3941182d95388b9a8c79fd3d2495942bc315d9d
James Dougherty <jjdougherty89@gmail.com>
2022-02-16 10:10:57 -0500

    Initial commit

---
+++ src/main/java/com/doughdesgin/doughmanagement/DoughManagementApplication.java
@@ -0,0 +1,13 @@
+package com.doughdesgin.doughmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoughManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoughManagementApplication.class, args);
+	}
+
+}
